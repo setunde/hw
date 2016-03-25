663b4e0ed8c039a686a0f3e779c0c95c8ef70204
tamas_suli <tamas_suli@epam.com>
2016-03-25 11:27:13 +0100

    initial HW

---
+++ CandyShop/src/com/epam/Candy.java
@@ -0,0 +1,21 @@
+package com.epam;
+
+public class Candy extends Gifts {
+    private enum Taste {HONEY,MENTHOL,CLASSIC};
+    private Taste taste = Taste.CLASSIC;
+
+
+    public Candy() {
+        super("Classic candy",190);
+    }
+
+    public Candy(String name, int price, int quantity, Taste taste) {
+        super(name,price,quantity);
+        this.taste = taste;
+    }
+
+    @Override
+    public String getSpecialParameter() {
+        return String.valueOf(taste);
+    }
+}
